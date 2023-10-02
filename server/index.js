465640576a030541244d60896ebf7cc0491b7698
RS-DEVS <pedrodeveloper98@gmail.com>
2023-10-02 15:04:46 +0200

    Resolver conflictos

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Servidor Express escuchando en el puerto ${PORT}`);
 });
